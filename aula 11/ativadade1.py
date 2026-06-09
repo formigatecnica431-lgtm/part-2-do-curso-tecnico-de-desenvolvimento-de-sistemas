0217bc1177e6222e69b84d12ad7f7e503eef1db9
denzel moreira <formigatecnica431@gmail.com>
2026-06-09 19:52:40 -0300

    aula11

---
+++ aula 11/ativadade1.py
@@ -0,0 +1,9 @@
+from classProduto import Produto
+
+produto1 = Produto("Morangos de chocolate",1000,10)
+
+produto1.valor_total()
+
+produto1.valor_desconto()
+
+produto1.mostrar_dados()
---
+++ aula 11/classProduto.py
@@ -0,0 +1,21 @@
+class Produto():
+    def __init__(self,nome,preco,quantidade):
+        self.nome =  nome
+        self.preco = preco
+        self.quantidade = quantidade
+        
+    def valor_total(self):
+        valor_total_produto = self.preco * self.quantidade
+        print(f"Seu valor total é {valor_total_produto}")
+        
+    def valor_desconto(self,percentual):
+        self.preco -= self.preco * percentual / 100
+        
+    def mostrar_dados(self):
+        print(f"""
+{"="*20}Nota fiscal{"="*20}
+| Produto:{self.nome}
+| Valor Total:{self.preco}
+| Quantidade:{self.quantidade}
+{"="*80}
+""")
